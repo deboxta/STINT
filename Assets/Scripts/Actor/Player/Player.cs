23b350f9e4781f4d162a82510832b0d31fa88368
Anthony <turtwig-_-antho@hotmail.com>
2019-10-02 14:48:39 -0400

    commit fix

--- Assets/Scripts/Actor/Player/Player.cs
+++ Assets/Scripts/Actor/Player/Player.cs
@@ -1,7 +1,32 @@
-﻿namespace Actor
+﻿using System;
+using UnityEngine;
+using UnityEngine.Experimental.PlayerLoop;
+using XInputDotNetPure;
+namespace Game
 {
-    public class Player
+    public class Player : MonoBehaviour
     {
-        
+        private PlayerMover mover;
+        static private GamePadState gamePad = GamePad.GetState(PlayerIndex.One);
+        private bool isGrounded;
+
+
+        private void Awake()
+        {
+            mover = GetComponent<PlayerMover>();
+            isGrounded = true;
+        }
+
+        private void Update()
+        {
+            var direction = Vector2.zero;
+            if (Input.GetKey(KeyCode.D) ||
+                gamePad.ThumbSticks.Left.X > 0)
+                direction += Vector2.right;
+            if (Input.GetKey(KeyCode.A) ||
+                gamePad.ThumbSticks.Left.X < 0)
+                direction += Vector2.left;
+            mover.Move(direction);
+        }
     }
 }
