3b48bd0ebfc8f212c3144f8267b5413243bcaa12
Anthony <turtwig-_-antho@hotmail.com>
2019-10-03 09:22:22 -0400

    jump force

--- Assets/Scripts/Actor/Player/PlayerJumpGravity.cs
+++ Assets/Scripts/Actor/Player/PlayerJumpGravity.cs
@@ -1,13 +1,12 @@
-﻿using System;
-using UnityEngine;
+﻿using UnityEngine;
 using XInputDotNetPure;
 
 namespace Game
 {
     public class PlayerJumpGravity : MonoBehaviour
     {
-        private int fallGravity = 3;
-        private int tinyJumpGravity = 2;
+        [SerializeField] private int fallGravity = 1;
+        [SerializeField] private int tinyJumpGravity = 5;
 
         private Rigidbody2D rigidbody2D;
 
@@ -20,11 +19,11 @@ namespace Game
         {
             if (rigidbody2D.velocity.y < 0)
             {
-                rigidbody2D.velocity += Vector2.up * Physics2D.gravity.y * (fallGravity - 1) * Time.deltaTime;
+                rigidbody2D.velocity += Time.deltaTime * Physics2D.gravity.y * (fallGravity - 1) * Vector2.up;
             }
             else if (rigidbody2D.velocity.y > 0 && GamePad.GetState(PlayerIndex.One).Buttons.A == ButtonState.Released)
             {
-                rigidbody2D.velocity += Vector2.up * Physics2D.gravity.y * (tinyJumpGravity - 1) * Time.deltaTime;
+                rigidbody2D.velocity += Time.deltaTime * Physics2D.gravity.y * (tinyJumpGravity - 1) * Vector2.up;
             }
         }
     }
