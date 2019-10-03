0e1d2d960a831994759d24b8db6fdfe8defd5149
Anthony <turtwig-_-antho@hotmail.com>
2019-10-03 08:25:38 -0400

    tests

---
+++ Assets/Scripts/Actor/Player/PlayerJumpGravity.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using UnityEngine;
+using XInputDotNetPure;
+
+namespace Game
+{
+    public class PlayerJumpGravity : MonoBehaviour
+    {
+        private int fallGravity = 3;
+        private int tinyJumpGravity = 2;
+
+        private Rigidbody2D rigidbody2D;
+
+        private void Awake()
+        {
+            rigidbody2D = GetComponent<Rigidbody2D>();
+        }
+
+        private void Update()
+        {
+            if (rigidbody2D.velocity.y < 0)
+            {
+                rigidbody2D.velocity += Vector2.up * Physics2D.gravity.y * (fallGravity - 1) * Time.deltaTime;
+            }
+            else if (rigidbody2D.velocity.y > 0 && GamePad.GetState(PlayerIndex.One).Buttons.A == ButtonState.Released)
+            {
+                rigidbody2D.velocity += Vector2.up * Physics2D.gravity.y * (tinyJumpGravity - 1) * Time.deltaTime;
+            }
+        }
+    }
+}
