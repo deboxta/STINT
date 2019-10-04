d0f9456ffeba720ad1d52c6643e7487450ed5a71
Mathieu Boutet <matbout@mail.com>
2019-10-04 14:34:47 -0400

    Refactored timeline stuff

---
+++ Assets/Scripts/Play/Events/TimelineChangedEventChannel.cs
@@ -0,0 +1,18 @@
+﻿using Harmony;
+using UnityEngine;
+
+namespace Game
+{
+    [Findable(R.S.Tag.TimeController)]
+    public class TimelineChangedEventChannel : MonoBehaviour
+    {
+        public event TimelineChangedEventHandler OnTimelineChanged;
+
+        public void NotifyTimelineChanged()
+        {
+            if (OnTimelineChanged != null) OnTimelineChanged();
+        }
+        
+        public delegate void TimelineChangedEventHandler();
+    }
+}
