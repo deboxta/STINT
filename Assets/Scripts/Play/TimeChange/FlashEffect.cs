09b58cc6836e30ec9418f9c053c72a57d19ed25c
Jeammy <jeammy32@gmail.com>
2019-10-07 10:43:49 -0400

    Base of FlashEffect.

--- Assets/Scripts/Play/TimeChange/FlashEffect.cs
+++ Assets/Scripts/Play/TimeChange/FlashEffect.cs
@@ -1,7 +1,14 @@
-﻿namespace Game
+﻿using Harmony;
+using UnityEngine;
+
+namespace Game
 {
-    public class FlashEffect
+    [Findable(R.S.Tag.MainController)]
+    public class FlashEffect : MonoBehaviour
     {
+        [SerializeField] private Animator animator;
+        private static readonly int FADE_OUT = Animator.StringToHash("Fade-out");
+        
         
     }
 }
