5a62155640853893fcc07d05b1641367125b5133
Anthony <turtwig-_-antho@hotmail.com>
2019-10-07 17:36:31 -0400

    Gravity

--- Assets/Scripts/Play/Actor/Player/PlayerMover.cs
+++ Assets/Scripts/Play/Actor/Player/PlayerMover.cs
@@ -7,13 +7,21 @@ namespace Game
     {
         [SerializeField] private int horizontalSpeed = 5;
         [SerializeField] private int jumpForce = 10;
-
+        [SerializeField] private int gravityMultiplier = 1;
+        [SerializeField] private int fallGravityMultiplier = 5;
+        
 
         private Rigidbody2D rigidBody2D;
 
         private void Awake()
         {
             rigidBody2D = GetComponent<Rigidbody2D>();
+        }
+        
+        private void FixedUpdate()
+        {
+            if (rigidBody2D.velocity.y < 0)
+                rigidBody2D.velocity += Time.deltaTime * Physics2D.gravity.y * gravityMultiplier * Vector2.up;
         }
 
         public void Move(Vector2 direction)
@@ -28,6 +36,11 @@ namespace Game
             var velocity = rigidBody2D.velocity;
             velocity.y = jumpForce;
             rigidBody2D.velocity = velocity;
+        }
+        
+        public void Fall()
+        {
+            rigidBody2D.velocity += Time.deltaTime * Physics2D.gravity.y * fallGravityMultiplier * Vector2.up;
         }
     }
 }
