85ca17b01d8aef811eae71582f45277126ee70ad
Jeammy <jeammy32@gmail.com>
2019-10-09 17:33:51 -0400

    last commit before merge

--- Assets/Scripts/Play/Game/TimeTravel/ObjectSuperpositionHandler.cs
+++ Assets/Scripts/Play/Game/TimeTravel/ObjectSuperpositionHandler.cs
@@ -17,8 +17,8 @@ namespace Game
             var playerBounds = player.GetComponentInChildren<BoxCollider2D>().bounds;
             
             //TODO: At this time the way we compare the bounds of the player is working, but it would be better to check two specific points later.
-            //Vector3 topLeftPosition = new Vector3(playerBounds.center.x - playerBounds.extents.x,playerBounds.center.y - playerBounds.extents.y);
-            //Vector3 bottomRightPosition = new Vector3(playerBounds.center.x + playerBounds.extents.x,playerBounds.center.y + playerBounds.extents.y);
+            //Vector3 bottomLeftPosition = new Vector3(playerBounds.center.x - playerBounds.extents.x,playerBounds.center.y - playerBounds.extents.y);
+            //Vector3 topRightPosition = new Vector3(playerBounds.center.x + playerBounds.extents.x,playerBounds.center.y + playerBounds.extents.y);
             
             if (other.CompareTag(R.S.Tag.DeathZone) &&
                 other.bounds.Contains(playerBounds.center))
