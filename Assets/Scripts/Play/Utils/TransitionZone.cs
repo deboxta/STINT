1f1ec4c7565fb9dc6b7b641090c7252723d09442
Sebastien Arsenault <sebastienarsenault1999@gmail.com>
2019-11-06 17:28:18 -0500

    Commit to change branch

--- Assets/Scripts/Play/Utils/TransitionZone.cs
+++ Assets/Scripts/Play/Utils/TransitionZone.cs
@@ -7,61 +7,62 @@ namespace Game
 {
     public class TransitionZone : MonoBehaviour
     {
-        [SerializeField] private Player player;
         [SerializeField] private CinemachineVirtualCamera cinemachineVirtualCamera;
         [SerializeField] private CompositeCollider2D roomOne;
         [SerializeField] private CompositeCollider2D roomTwo;
+        [SerializeField] private bool moveHorozontaly = true;
 
         private ISensor<Player> playerSensor;
-        private BoxCollider2D transitionBox;
 
         private void Awake()
         {
             playerSensor = GetComponent<Sensor>().For<Player>();
         }
 
-        private IEnumerator Start()
-        {
-            yield return null;
-
-            transitionBox = GetComponent<BoxCollider2D>();
-        }
-
         private void OnEnable()
         {
             playerSensor.OnSensedObject += OnPlayerSensed;
-            playerSensor.OnSensedObject += OnPlayerUnSensed;
+            playerSensor.OnUnsensedObject += OnPlayerUnSensed;
         }
 
         private void OnDisable()
         {
             playerSensor.OnSensedObject -= OnPlayerSensed;
-            playerSensor.OnSensedObject -= OnPlayerUnSensed;
+            playerSensor.OnUnsensedObject -= OnPlayerUnSensed;
         }
 
         private void OnPlayerSensed(Player sensedPlayer)
         {
-            player.GetComponent<Rigidbody2D>().isKinematic = true;
+            var sensedPlayerRigidbody2D = sensedPlayer.GetComponent<Rigidbody2D>();
+            sensedPlayerRigidbody2D.isKinematic = true;
 
-            var cinemachineConfiner = cinemachineVirtualCamera.GetComponent<CinemachineConfiner>();
-            cinemachineConfiner.enabled = false;
-        }
-
-        private void OnPlayerUnSensed(Player sensedPlayer)
-        {
-            player.GetComponent<Rigidbody2D>().isKinematic = false;
+            if (moveHorozontaly)
+            {
+                var playerVelocity = sensedPlayerRigidbody2D.velocity;
+                
+                playerVelocity.y = 0;
+                sensedPlayerRigidbody2D.velocity = playerVelocity;
+            }
+            else
+            {
+                var playerVelocity = sensedPlayerRigidbody2D.velocity;
+                
+                playerVelocity.x = 0;
+                sensedPlayerRigidbody2D.velocity = playerVelocity;
+            }
+            
+            sensedPlayer.GetComponent<PlayerInput>().enabled = false;
 
             var cinemachineConfiner = cinemachineVirtualCamera.GetComponent<CinemachineConfiner>();
             cinemachineConfiner.m_BoundingShape2D =
                 (cinemachineConfiner.m_BoundingShape2D == roomOne) ? roomTwo : roomOne;
-
-            cinemachineConfiner.enabled = true;
         }
 
-        /*private IEnumerator MovePlayer()
+        private void OnPlayerUnSensed(Player sensedPlayer)
         {
-            
-        }*/
-
+            var sensedPlayerRigidbody2D = sensedPlayer.GetComponent<Rigidbody2D>();
+            sensedPlayerRigidbody2D.isKinematic = false;
+            sensedPlayer.GetComponent<PlayerInput>().enabled = true;
+        }
     }
 }
