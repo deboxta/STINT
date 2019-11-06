e42c2028d94efc8b614e3c19581f9db50275d0ef
Yannick Côté <yannick.c.pro.a@gmail.com>
2019-11-06 16:53:44 -0500

    Chapter 5 first room started.

---
+++ Assets/Scripts/Play/Actor/Gravity/Gravity.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Harmony;
+using UnityEngine;
+
+namespace Game
+{
+    public class Gravity : MonoBehaviour
+    {
+        [SerializeField] private Player player = null;
+
+        public static float force;
+
+        private MainController mainController;
+        private PointEffector2D pointEffector2D;
+        
+        // Start is called before the first frame update
+        void Start()
+        {
+            mainController = Finder.MainController;
+            player = Finder.Player;
+            pointEffector2D = GetComponent<PointEffector2D>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            //player.GetComponent	<Rigidbody>()
+            force = pointEffector2D.forceMagnitude;
+        }
+    } 
+}
+
