45378b4b99dc334c80523d85e91f167cd944c90b
Sebastien Arsenault <sebastienarsenault1999@gmail.com>
2019-11-08 15:48:01 -0500

    Added a SuccesDetector to get notify when a succes is completed and a event channel GameWonEventChannel

---
+++ Assets/Scripts/Play/Succes/SuccesDetector.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Harmony;
+using UnityEngine;
+
+namespace Game
+{
+    public class SuccesDetector : MonoBehaviour
+    {
+        private PlayerDeathEventChannel playerDeathEventChannel;
+        private GameWonEventChannel gameWonEventChannel;
+
+        private void Awake()
+        {
+            playerDeathEventChannel = Finder.PlayerDeathEventChannel;
+            gameWonEventChannel = Finder.GameWonEventChannel;
+
+            CheckAlreadyUnlockedSucces();
+        }
+
+        private void OnEnable()
+        {
+            playerDeathEventChannel.OnPlayerDeath += OnPlayerDeath;
+            gameWonEventChannel.OnGameWon += OnGameWon;
+        }
+
+        private void OnDisable()
+        {
+            playerDeathEventChannel.OnPlayerDeath -= OnPlayerDeath;
+            gameWonEventChannel.OnGameWon -= OnGameWon;
+        }
+
+        private void OnPlayerDeath()
+        {
+            if (expr)
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        private void OnGameWon()
+        {
+            if (expr)
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        private void CheckAlreadyUnlockedSucces()
+        {
+            if (gameWon && neverDied)
+            {
+                gameWonEventChannel.OnGameWon -= OnGameWon;
+            }
+
+            if (expr)
+            {
+                
+            }
+        }
+    }
+}
