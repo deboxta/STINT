0051f5e0f858c685c236fd7c813ff28e69f3f457
Anthony <turtwig-_-antho@hotmail.com>
2019-11-25 17:20:43 -0500

    Ghost of everything in the other dimension

---
+++ Assets/Scripts/Play/Game/TimeTravel/TimeLineChangeableLevelVisual.cs
@@ -0,0 +1,65 @@
+﻿using Harmony;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+namespace Game
+{
+    public class TimeLineChangeableLevelVisual : MonoBehaviour
+    {
+        private TimelineChangedEventChannel timelineChangedEventChannel;
+
+        private const float ALPHA_OF_COLOR = 0.35f;
+        private TilemapCollider2D tilemapCollider2D;
+        private Tilemap tilemap;
+        private Color originalColor;
+
+        private void Awake()
+        {
+            timelineChangedEventChannel = Finder.TimelineChangedEventChannel;
+            tilemap = GetComponent<Tilemap>();
+            tilemapCollider2D = GetComponent<TilemapCollider2D>();
+            originalColor = tilemap.color;
+        }
+
+        private void OnEnable()
+        {
+            timelineChangedEventChannel.OnTimelineChanged += TimelineChanged;
+        }
+
+        private void OnDisable()
+        {
+            timelineChangedEventChannel.OnTimelineChanged -= TimelineChanged;
+        }
+
+        protected void TimelineChanged()
+        {
+            switch (Finder.TimelineController.CurrentTimeline)
+            {
+                case Timeline.Primary:
+                    if (CompareTag(R.S.Tag.MainTimeline))
+                        EnableCollider();
+                    else
+                        DisableCollider();
+                    break;
+                case Timeline.Secondary:
+                    if (CompareTag(R.S.Tag.SecondaryTimeline))
+                        EnableCollider();
+                    else
+                        DisableCollider();
+                    break;
+            }
+        }
+
+        private void EnableCollider()
+        {
+            tilemapCollider2D.enabled = true;
+            tilemap.color = originalColor;
+        }
+
+        private void DisableCollider()
+        {
+            tilemapCollider2D.enabled = false;
+            tilemap.color = new Color(originalColor.r, originalColor.g, originalColor.b, ALPHA_OF_COLOR);
+        }
+    }
+}
