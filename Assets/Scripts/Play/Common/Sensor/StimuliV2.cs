98b5a7e9d7ebbfd48caaca6b1308299a676054fd
Mathieu Boutet <matbout@mail.com>
2019-11-26 17:35:01 -0500

    Created sensor V2

---
+++ Assets/Scripts/Play/Common/Sensor/StimuliV2.cs
@@ -0,0 +1,39 @@
+﻿using Harmony;
+using UnityEngine;
+
+namespace Game
+{
+    // Author : Mathieu Boutet
+    public sealed class StimuliV2 : MonoBehaviour
+    {
+        public event StimuliV2EventHandler OnDestroyed;
+
+        private void Awake()
+        {
+            InitCollider();
+            SetSensorLayer();
+        }
+
+        private void OnDestroy()
+        {
+            NotifyDestroyed();
+        }
+
+        private void InitCollider()
+        {
+            gameObject.GetComponent<Collider2D>().isTrigger = true;
+        }
+
+        private void SetSensorLayer()
+        {
+            gameObject.layer = LayerMask.NameToLayer(R.S.Layer.Sensor);
+        }
+
+        private void NotifyDestroyed()
+        {
+            if (OnDestroyed != null) OnDestroyed(transform.parent.gameObject);
+        }
+    }
+
+    public delegate void StimuliV2EventHandler(GameObject otherObject);
+}
